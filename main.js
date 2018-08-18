7001e04f05ab4d82c4bcac83a996138873ce8b30
Sarah Han <sarah.s.han@gmail.com>
2018-08-17 18:00:46 -0700

    staff kitchen modified

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 
 
 var marriot = new Hotel();
+
+// In main.js, add 5 rooms to your hotel
+var room101 = new Room(marriot);
+var room201 = new Room(marriot);
+var room301 = new Room(marriot);
+var room401 = new Room(marriot);
+var room501 = new Room(marriot);
+
+// In main.js add 4 occupants to your hotel
+var jaytrin = new Customer('Jaytrin');
+var torie = new Customer('Torie');
+var chaise = new Customer('Chaise');
+var sarah = new Customer('Sarah');
+
+// In main.js add 1 staff to your
+var brett = new Staff(marriot);
--- staff.js
+++ staff.js
@@ -5,8 +5,14 @@ class Staff{
 		this.busy = false;
 		this.hotel = currentHotel;
 	}
-	handleKitchenRequest(){
-		
+	handleKitchenRequest( food, room ){
+		setTimeout(completeKitchenRequest, (Math.floor(Math.random() * 3000), room);
+		//wait a random time up to 30 seconds and then call the staff's complete
+		this.busy = true;
+	}
+	completeKitchenRequest ( room ) {
+        //set the room’s occupant’s isHungry to false
+		room.occupant.ishungry = false;
 	}
 
 }
